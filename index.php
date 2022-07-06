dabab2cfea8b848093f3c4e91b974ab52d4fa2d2
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-07-06 14:08:06 +0300

    Taking existing reports from DB

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbh = new PDO('mysql:host=localhost; dbname=laravel; charset=utf8mb4', 'root', '');
+$dbh = new PDO('mysql:host=localhost; dbname=laravel; charset=utf8mb4', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 $sql = $dbh->query("SELECT * FROM tables");
 
@@ -14,24 +14,41 @@ $weekly = [];
 $monthly = [];
 $quarterly = [];
 
-foreach ($data as $table) {
+foreach ($data as $key => $table) {
    if ($table['periodicity'] == 1) {
-      $daily[] = $table;
-      $daily['report_table'] = 'daily_report';
+      $daily[$key] = $table;
+      $daily[$key]['report_table'] = 'daily_reports';
    } elseif ($table['periodicity'] == 2) {
-      $weekly[] = $table;
-      $weekly['report_table'] = 'weekly_report';
+      $weekly[$key] = $table;
+      $weekly[$key]['report_table'] = 'weekly_reports';
    } elseif ($table['periodicity'] == 3) {
-      $monthly[] = $table;
-      $monthly['report_table'] = 'monthly_report';
+      $monthly[$key] = $table;
+      $monthly[$key]['report_table'] = 'monthly_reports';
    } elseif ($table['periodicity'] == 4) {
-      $quarterly[] = $table;
-      $quarterly['report_table'] = 'quarterly_report';
+      $quarterly[$key] = $table;
+      $quarterly[$key]['report_table'] = 'quarterly_reports';
    }
 }
 
-function reports($periodicity) {
-   var_dump($periodicity);
+function reports($periodicity, $dbh) {
+   date_default_timezone_set('Europe/Moscow');
+   $target_date = date('Y-m-d, H:i:s');
+   foreach ($periodicity as $period) {
+      $periodicity_report = $dbh->query("SELECT * FROM " . $period['report_table'] . "")->fetchAll();
+      foreach ($periodicity_report as $reports) {
+         if ($reports['table_uuid'] == $period['table_uuid']) {
+            foreach(json_decode($period['departments']) as $department) {
+               if($department == $reports['user_dep']) {
+                  var_dump($reports);
+
+               }
+            }
+         } else {
+            $period['table_uuid'];
+         }
+      }
+   }
 }
 
-reports($daily);
+reports($daily, $dbh);
+
