c875f758186a3ca2017ffc7925523885c10b8818
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-07-07 14:52:46 +0300

    Start making function for filling all periodic tables

--- index.php
+++ index.php
@@ -2,37 +2,41 @@
 
 $dbh = new PDO('mysql:host=localhost; dbname=laravel; charset=utf8mb4', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$sql = $dbh->query("SELECT * FROM tables");
-
-$data = $sql->fetchAll();
-
-//$sql = null;
-//$dbh = null;
+$data = $dbh->query("SELECT * FROM tables")->fetchAll();
 
 $daily = [];
 $weekly = [];
 $monthly = [];
 $quarterly = [];
 
-foreach ($data as $key => $table) {
-   if ($table['periodicity'] == 1) {
-      $daily[$key] = $table;
-      $daily[$key]['report_table'] = 'daily_reports';
-   } elseif ($table['periodicity'] == 2) {
-      $weekly[$key] = $table;
-      $weekly[$key]['report_table'] = 'weekly_reports';
-   } elseif ($table['periodicity'] == 3) {
-      $monthly[$key] = $table;
-      $monthly[$key]['report_table'] = 'monthly_reports';
-   } elseif ($table['periodicity'] == 4) {
-      $quarterly[$key] = $table;
-      $quarterly[$key]['report_table'] = 'quarterly_reports';
+
+function launching_script($data, $dbh) {
+   $target_date = date('Y-m-d');
+
+   foreach ($data as $key => $table) {
+      if ($table['periodicity'] == 1) {
+         $daily[$key] = $table;
+         $daily[$key]['report_table'] = 'daily_reports';
+      } elseif ($table['periodicity'] == 2) {
+         $weekly[$key] = $table;
+         $weekly[$key]['report_table'] = 'weekly_reports';
+      } elseif ($table['periodicity'] == 3) {
+         $monthly[$key] = $table;
+         $monthly[$key]['report_table'] = 'monthly_reports';
+      } elseif ($table['periodicity'] == 4) {
+         $quarterly[$key] = $table;
+         $quarterly[$key]['report_table'] = 'quarterly_reports';
+      }
    }
+
+   reports($daily, $dbh, $target_date);
+
+
 }
 
-function reports($periodicity, $dbh) {
+function reports($periodicity, $dbh, $target_date) {
    date_default_timezone_set('Europe/Moscow');
-   $target_date = date('Y-m-d');
+
    $clear_reports = [];
    $counter = 0;
    foreach ($periodicity as $period) {
@@ -70,4 +74,6 @@ function reports($periodicity, $dbh) {
    }
 }
 
-reports($daily, $dbh);
+launching_script($data, $dbh);
+
+
